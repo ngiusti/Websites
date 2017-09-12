74e525906428d08cd4ec03dc24f4e10dbf20b6f0
Nicholas Giusti <nicholas.giusti89@gmail.com>
2017-09-12 13:12:43 -0700

    funiture page
    Furniture page For fun.

---
+++ funiturePage/funiturePage/resources/js/scripts.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+    var delayMillis = 1000;
+
+    $('.js-truckIcon').waypoint(function (direction) {
+        $('.js-truckIcon').addClass('animated slideInLeft');
+    }, {
+            offset: '50%'
+        });
+    
+    setTimeout(function () {
+        $('.js--deal-pulse1').waypoint(function (direction) {
+            $('.js--deal-pulse1').addClass('animated pulse');
+
+
+        }, {
+                offset: '50%'
+        });
+    }, 1 * delayMillis);
+    setTimeout(function () {
+        $('.js--deal-pulse2').waypoint(function (direction) {
+            $('.js--deal-pulse2').addClass('animated pulse');
+
+
+        }, {
+                offset: '50%'
+            });
+    }, 2 * delayMillis);
+    setTimeout(function () {
+        $('.js--deal-pulse3').waypoint(function (direction) {
+            $('.js--deal-pulse3').addClass('animated pulse');
+
+
+        }, {
+                offset: '50%'
+            });
+    }, 3 * delayMillis);
+
+    $('.js--stock').waypoint(function (direction) {
+        $('.js--stock').addClass('animated rubberBand');
+    }, {
+            offset: '50%'
+        });
+});
+
